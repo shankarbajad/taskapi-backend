5058d090e4c1ab39c5fc665455fdab686166d934
hitesh kanwar <hitesh.gurjar786@gmail.com>
2020-10-27 21:45:25 +0530

    Initial commit

--- task/settings.py
+++ task/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
